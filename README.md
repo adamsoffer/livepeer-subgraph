5a1160ab0155fabd55d100d38df55e91aeef317e
Adam Soffer <adam@soffer.xyz>
2018-08-14 13:42:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Livepeer
+Livepeer subgraph manifest for The Graph
---
+++ mappings/bondingManager.ts
@@ -0,0 +1,38 @@
+
+// Respond to transcoder added events
+export function transcoderUpdated(event: TranscoderUpdate): void {
+  let bondingManager = BondingManager.bind(event.address, event.blockHash)
+  let roundsManager = RoundsManager.bind(Address.fromString("0x511bc4556d823ae99630ae8de28b9b80df90ea2e"), event.blockHash)
+
+  let currentRound = roundsManager.currentRound()
+  let transcoderHash = event.params.transcoder.toHex()
+  let transcoderAddress = event.params.transcoder
+  let registered = event.params.registered
+  let transcoderInfo = bondingManager.getTranscoder(transcoderAddress)
+  let lastRewardRound = transcoderInfo.value0
+  let rewardCut = transcoderInfo.value1
+  let feeShare = transcoderInfo.value2
+  let pricePerSegment = transcoderInfo.value3
+  let pendingRewardCut = transcoderInfo.value4
+  let pendingFeeShare = transcoderInfo.value5
+  let pendingPricePerSegment = transcoderInfo.value6
+  let totalStake = bondingManager.transcoderTotalStake(transcoderAddress)
+  
+  // Create transcoder entity
+  let transcoder = new Entity()
+  transcoder.setString('name', "Transcoder")
+  transcoder.setString('id', transcoderHash)
+  transcoder.setAddress('address', transcoderAddress)
+  transcoder.setU256('pendingRewardCut', pendingRewardCut)
+  transcoder.setU256('pendingFeeShare', pendingFeeShare)
+  transcoder.setU256('pendingPricePerSegment', pendingPricePerSegment)
+  transcoder.setU256('totalStake', totalStake)
+  transcoder.setU256('lastRewardRound', lastRewardRound)
+  transcoder.setU256('currentRound', currentRound)
+  transcoder.setBoolean('registered', registered)
+  
+
+  // Apply store updates
+  let store = Store.bind(event.blockHash)
+  store.set('Transcoder', transcoderHash, transcoder)
+}
