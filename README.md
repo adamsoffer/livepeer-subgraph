687cbfd5c460aba628c0d2652e449eb3ffe08bab
Adam Soffer <adam@soffer.space>
2019-01-26 13:08:41 -0500

    Add delegator and share entity and update all dependencies

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
-# example-subgraph
-An example to help you get started with The Graph
+# Livepeer Subgraph
+
+[![Discord](https://img.shields.io/discord/423160867534929930.svg?style=flat-square)](https://discord.gg/7wRSUGX)
+[![GitHub issues](https://img.shields.io/github/issues/livepeer/livepeerjs/subgraph.svg?style=flat-square)](https://github.com/livepeer/livepeerjs/labels/subgraph)
+
+This package contains the source code for the Livepeer Subgraph, a project for
+indexing and querying Livepeer data from the Ethereum blockchain using [The Graph](https://thegraph.com).
+
+## Quick Start
+
+### Running a local Graph Node with Docker Compose
+
+The quickest way to run a Graph Node locally is to use the
+[graph-node docker image](https://hub.docker.com/r/graphprotocol/graph-node/).
+
+1. Install [Docker](https://docs.docker.com) and [Docker Compose](https://docs.docker.com/compose/install/)
+2. In the root of this project run `docker-compose up`
+
+This command will look for the `docker-compose.yml` file and automatically provision a server with rust, postgres, and ipfs, and
+spin up a graph node with a GraphiQL interface at `http://127.0.0.1:8000/`.
+Congrats, you're now ready to build and deploy the Livepeer subgraph.
+
+### Building and Deploying the Livepeer Subgraph
+
+Build types
+`yarn codegen`
+
+Compile subgraph
+`yarn build`
+
+Deploy subgraph
+`yarn deploy`
+
+After downloading the latest blocks from Ethereum, you should begin to see
+Livepeer smart contract events flying in. Open a GraphiQL browser at
+localhost:8000 to query the Graph Node.
+
+Here's an example query for fetching Livepeer transcoders:
+
+```
+query {
+  transcoders {
+    id
+    rewardCut
+    feeShare
+    pricePerSegment
+    pendingRewardCut
+    pendingFeeShare
+    pendingPricePerSegment
+    totalStake
+    lastRewardRound
+    active
+    status
+    rewards {
+      rewardTokens
+      round {
+        id
+      }
+    }
+  }
+}
+```
+
+Here's another example query for fetching rounds:
+
+```
+query {
+  rounds {
+    id
+    rewards {
+      rewardTokens
+      transcoder {
+        id
+      }
+    }
+  }
+}
+```
+
+### Explorer Integration
+
+Once you've got your Graph Node running and successfully deployed the Livepeer
+Subgraph, you can integrate it with the [Explorer](https://github.com/livepeer/livepeerjs/tree/master/packages/explorer) by setting the
+its environment variable `REACT_APP_LIVEPEER_SUBGRAPH` inside `.env.development` to
+your graphql endpoint.
+
+For example:
+`REACT_APP_LIVEPEER_SUBGRAPH=http://localhost:8000/subgraphs/name/livepeer/graphql`
+
+The explorer will begin quering indexed transcoder data from the Graph Node.
+Note that if you shutdown the Graph Node, the Explorer will gracefully fallback
+to querying the transcoder data from the blockchain directly.
---
+++ src/mappings/bondingManager_deprecated.ts
@@ -0,0 +1,103 @@
+import { Address } from '@graphprotocol/graph-ts'
+
+// Import event types from the registrar contract ABIs
+import {
+  BondingManager,
+  Bond as Bond_deprecated,
+  Unbond as Unbond_deprecated
+} from '../types/BondingManager_deprecated/BondingManager'
+
+// Import entity types generated from the GraphQL schema
+import { Transcoder, Delegator } from '../types/schema'
+
+export function bond_deprecated(event: Bond_deprecated): void {
+  let bondingManager = BondingManager.bind(event.address)
+  let delegateAddress = event.params.delegate
+  let delegatorAddress = event.params.delegator
+  let delegateTotalStake = bondingManager.transcoderTotalStake(delegateAddress)
+
+  // Get delegator data
+  let delegatorData = bondingManager.getDelegator(delegatorAddress)
+  let startRound = delegatorData.value4
+  let bondedAmount = delegatorData.value0
+
+  // Create delegate if it does not yet exist
+  let delegate = Transcoder.load(delegateAddress.toHex())
+  if (delegate == null) {
+    delegate = new Transcoder(delegateAddress.toHex())
+  }
+  // Create delegator if it does not yet exist
+  let delegator = Delegator.load(delegatorAddress.toHex())
+  if (delegator == null) {
+    delegator = new Delegator(delegatorAddress.toHex())
+  }
+
+  if (delegate.delegators == null) {
+    delegate.delegators = new Array<string>()
+  }
+
+  // Add delegator to delegate if it's not already
+  let delegators = delegate.delegators
+  let i = delegators.indexOf(delegatorAddress.toHex())
+  if (i == -1) {
+    delegators.push(delegatorAddress.toHex())
+    delegate.delegators = delegators
+  }
+  delegator.delegate = delegateAddress.toHex()
+
+  // Update delegator bonded amount
+  delegator.bondedAmount = bondedAmount
+
+  // Update delegator's start round
+  delegator.startRound = startRound.toString()
+
+  // Update delegate's total stake
+  delegate.totalStake = delegateTotalStake
+
+  delegator.save()
+  delegate.save()
+}
+
+export function unbond_deprecated(event: Unbond_deprecated): void {
+  let bondingManager = BondingManager.bind(event.address)
+  let delegatorAddress = event.params.delegator
+  let delegator = Delegator.load(delegatorAddress.toHex())
+
+  // delegate is not emitted in the deprecated event so grab it from delegator
+  let delegateAddress = Address.fromString(delegator.delegate)
+  let delegate = Transcoder.load(delegateAddress.toHex())
+  if (delegate == null) {
+    delegate = new Transcoder(delegateAddress.toHex())
+  }
+
+  let totalStake = bondingManager.transcoderTotalStake(delegateAddress)
+
+  // Get delegator data
+  let delegatorData = bondingManager.getDelegator(delegatorAddress)
+  let startRound = delegatorData.value4
+  let bondedAmount = delegatorData.value0
+
+  // Update delegate's total stake
+  delegate.totalStake = totalStake
+
+  // Update delegator's bonded amount
+  delegator.bondedAmount = bondedAmount
+
+  // Remove delegator from delegate
+  let delegators = delegate.delegators
+  if (delegators != null) {
+    let i = delegators.indexOf(delegatorAddress.toHex())
+    delegators.splice(i, 1)
+    delegate.delegators = delegators
+  }
+
+  // Remote delegate from delegator
+  delegator.delegate = null
+
+  // Update delegator's start round
+  delegator.startRound = startRound.toString()
+
+  // Apply store updates
+  delegator.save()
+  delegate.save()
+}
